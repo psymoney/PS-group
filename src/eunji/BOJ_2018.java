5291a2685ccf6f2179b28cf795178a57e166877e
user <yenje7@naver.com>
2023-04-24 23:49:36 +0900

    수들의 합5

---
+++ src/eunji/BOJ_2018.java
@@ -0,0 +1,33 @@
+package eunji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2018{
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int N = Integer.parseInt(str);
+		
+		int start = 1;
+		int end = 1;
+		int cnt = 0;
+		int sum = 1;
+		
+		while(start <= end){
+		    if(sum == N) cnt++;
+		    if(sum < N){
+		        end++;
+		        sum+=end;
+		    }else if(sum >= N){
+		        sum -= start;
+		        start++;
+		    };
+		}
+ 		
+		System.out.println(cnt);
+ 		
+	}
+}
