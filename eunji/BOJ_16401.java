d51794348876ce3098a7844ba59098e97efb088c
yeoeunji <yenje7@naver.com>
2023-05-05 22:23:20 +0900

    과자 나눠주기

---
+++ eunji/BOJ_16401.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_16401 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str[] = br.readLine().split(" ");
+		int M = Integer.parseInt(str[0]);
+		int N = Integer.parseInt(str[1]);
+		
+		String arr_str[] = br.readLine().split(" ");
+		int arr[]= new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(arr_str[i]);
+		}
+		
+		int min=1;
+		int max=arr[N-1];
+		int mid=(min+max)/2;
+		int cnt=0;
+		
+		while(min<max) {
+			cnt=0;
+			mid=(min+max)/2;
+			for(int i=0;i<N;i++) {
+				cnt+=arr[i]/mid;
+			}
+			
+			if(cnt<M) {
+				max=mid-1;
+				if(max==0) break;
+			}	
+			else min=mid+1;
+		}
+		
+		System.out.print(max);
+	}
+}
