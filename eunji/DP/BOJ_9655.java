1d598e7a4737b3fc234714add63d5723ee091402
yeoeunji <yenje7@naver.com>
2023-05-15 23:50:29 +0900

    돌 게임

--- eunji/DP/BOJ_9655.java
+++ eunji/DP/BOJ_9655.java
@@ -7,9 +7,10 @@ import java.io.InputStreamReader;
 public class BOJ_9655 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		
-	
+		if(N%2!=0) System.out.print("SK");
+		else System.out.print("CY");
 	}
 }
